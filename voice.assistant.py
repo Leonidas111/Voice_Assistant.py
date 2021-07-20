b5c521169dc5a061a3464b2049a916e86451cc69
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-07-20 13:08:38 -0500

    Local MAIN: def talk and voices

--- voice.assistant.py
+++ voice.assistant.py
@@ -6,16 +6,18 @@ listener = sr.Recognizer()
 engine = pyttsx3.init()
 
 voices = engine.getProperty("voices")
-engine.setProperty("voice",voices[1].id)
+engine.setProperty("voice",voices[2].id)
 
-engine.say("Buen dia Jefe")
-engine.runAndWait()
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
 try:
     with sr.Microphone() as source:
+        talk("Estoy a sus ordenes mi Lord;lo escucho atentamente")
         print("Listening ...")
         voice = listener.listen(source)
         rec = listener.recognize_google(voice)
-        print(rec)
+        talk(rec)
     with open ("./historysearchs.txt","a",encoding="utf-8") as history:
         history.write("\n")
         history.write(rec)
