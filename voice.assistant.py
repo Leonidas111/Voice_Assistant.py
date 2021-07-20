d0e2179d0311d15bdf2f55b0d62fc5e1ae11c083
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-07-20 14:46:46 -0500

    Local EDITION: version edition

--- voice.assistant.py
+++ voice.assistant.py
@@ -1,23 +1,33 @@
 import speech_recognition as sr
 import pyttsx3
 
-listener = sr.Recognizer()
 
+name = "alexa"
+listener = sr.Recognizer()
 engine = pyttsx3.init()
 
 voices = engine.getProperty("voices")
-engine.setProperty("voice",voices[1].id)
+engine.setProperty("voice",voices[2].id)
 
-engine.say("Buen dia Jefe")
-engine.runAndWait()
-try:
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def funlisten(rec):
     with sr.Microphone() as source:
-        print("Listening ...")
         voice = listener.listen(source)
         rec = listener.recognize_google(voice)
-        print(rec)
+        if name in rec:
+            talk("Estoy a sus ordenes mi Lord")
+            print("listening...")
     with open ("./historysearchs.txt","a",encoding="utf-8") as history:
         history.write("\n")
-        history.write(rec)
-except:
-    pass
+        history.write(rec) 
+
+def run():
+    reproduce = lambda:funlisten("Reproduce")
+    talk(reproduce)
+    print("reproduciendo....")
+
+if __name__=="__main__":
+    run()
