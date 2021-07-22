f968a57ce5ac80fb60ed56e281970e03c0c8ee1a
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-07-22 16:33:16 -0500

    Local MAIN: Version 01 voice assistant

--- voice.assistant.py
+++ voice.assistant.py
@@ -1,27 +1,46 @@
 import speech_recognition as sr
 import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
 
-name = "benjamin"
+name = "alexa"
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 
 voices = engine.getProperty("voices")
 engine.setProperty("voice",voices[2].id)
 
+
 def talk(text):
     engine.say(text)
     engine.runAndWait()
-try:
-    with sr.Microphone() as source:
-        voice = listener.listen(source)
-        rec = listener.recognize_google(voice)
-        rec = rec.lower()
-        if name in rec:
-            talk("Estoy a sus ordenes mi Lord")
-            talk(rec)
-    #Guarda en historysearchs.txt:
-    with open ("./historysearchs.txt","a",encoding="utf-8") as history:
-        history.write("\n")
-        history.write(rec)
-except:
-    pass
+
+def listen():
+    try:
+        with sr.Microphone() as source:
+            print("Escuchando...")
+            voice = listener.listen(source)
+            rec = listener.recognize_google(voice)
+            rec = rec.lower()
+            if name in rec:
+                rec = rec.replace(name, "")
+                print(rec)
+    except:
+        pass
+    return rec
+
+def run():
+    rec = listen()
+    if "reproduce" in rec:
+        music = rec.replace("reproduce"," ")
+        talk("reproduciendo" + rec)
+        pywhatkit.playonyt(music)
+    if "hora" in rec:
+        hora = datetime.datetime().now().strftime("%I:%M %p")
+        talk("son las" + hora)
+    elif "busca" in rec:
+        order = rec.replace("busca","")
+        info = wikipedia.summary(order,1)
+        talk(info)
+run()
