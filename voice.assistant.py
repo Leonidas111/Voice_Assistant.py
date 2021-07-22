874e40151394f461141f03f64139ea4af38645ec
Leandro Diaz <85961824+Leonidas111@users.noreply.github.com>
2021-07-22 18:26:52 -0500

    Local MAIN: V0.2 finish

--- voice.assistant.py
+++ voice.assistant.py
@@ -6,6 +6,7 @@ import wikipedia
 
 
 name = "sirvienta"
+flag = 1
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 
@@ -18,27 +19,34 @@ def talk(text):
     engine.say(text)
     engine.runAndWait()
 
-def funlisten(rec):
+def listen():
+    flag = 1
     try:
         with sr.Microphone() as source:
+            print("Escuhando...")
             voice = listener.listen(source)
             rec = listener.recognize_google(voice, language='es-ES')
+            rec = rec.lower()
             if name in rec:
-                talk("Estoy a sus ordenes mi Lord")
-                print("Escuhando...")
                 rec = rec.replace(name, '')
+                flag = run(rec)
             else:
                 talk("Solo atiendo al llamado por mi nombre")
+
         with open ("./historysearchs.txt","a",encoding="utf-8") as history:
             history.write("\n")
             history.write(rec)
     except:
         pass 
-def run():
-    if 'reproduce' in rec:
-        music = rec.replace('reproduce', '')
-        talk('Reproduciendo ' + music)
+def run(rec):
+    if "reproduce" in rec:
+        music = rec.replace("reproduce", "")
+        talk('Reproduciendo' + music)
         pywhatkit.playonyt(music)
+    elif "abre" in rec:
+        web = rec.replace("abre","")
+        talk("Abriendo" + web)
+        pywhatkit.search(web)
     elif 'hora' in rec:
         hora = datetime.datetime.now().strftime('%I:%M %p')
         talk("Son las " + hora)
@@ -47,4 +55,12 @@ def run():
         wikipedia.set_lang("es")
         info = wikipedia.summary(order, 1)
         talk(info)
-run()
+    elif 'exit' in rec:
+        flag = 0
+        talk("Saliendo...")
+    else:
+        talk("Vuelve a intentarlo, no reconozco: " + rec)
+    return flag
+
+while flag:
+    flag = listen()
